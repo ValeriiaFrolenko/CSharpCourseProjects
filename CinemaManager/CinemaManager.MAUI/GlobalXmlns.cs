e3dd1b5b785a074652ad9ea22bc29fad7da2c420
Valeriia Frolenko <valeriiiafrol@gmail.com>
2026-03-03 00:31:48 +0200

    Delete folder

--- CinemaManager/CinemaManager.MAUI/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CinemaManager.MAUI")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CinemaManager.MAUI.Pages")]
