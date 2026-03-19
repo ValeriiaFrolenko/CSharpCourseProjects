83d0836fb4ab7cf04a6035aa961340afc8589574
Valeriia Frolenko <valeriiiafrol@gmail.com>
2026-03-19 03:46:39 +0200

    Edit routing to nested paths

--- CinemaManager/CinemaManager/AppShell.xaml.cs
+++ CinemaManager/CinemaManager/AppShell.xaml.cs
@@ -7,7 +7,7 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
-        Routing.RegisterRoute(nameof(HallDetailsPage), typeof(HallDetailsPage));
-        Routing.RegisterRoute(nameof(SessionDetailsPage), typeof(SessionDetailsPage));
+        Routing.RegisterRoute($"{nameof(HallsPage)}/{nameof(HallDetailsPage)}", typeof(HallDetailsPage));
+        Routing.RegisterRoute($"{nameof(HallsPage)}/{nameof(HallDetailsPage)}/{nameof(SessionDetailsPage)}", typeof(SessionDetailsPage));
     }
 }
